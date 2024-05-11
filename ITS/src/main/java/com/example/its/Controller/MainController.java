5b361e5cc1d401c6d6ce0cf660d923be0ebcd06d
Aman <gr0315@cau.ac.kr>
2024-05-11 19:16:40 +0900

    init : initiate dependency of java project

---
+++ ITS/src/main/java/com/example/its/Controller/MainController.java
@@ -0,0 +1,25 @@
+package com.example.its.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/demo")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
