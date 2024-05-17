fe201ff795029eef228f33fbaba939a69cddcd0e
CloudNru <grutugy1025@gmail.com>
2024-05-17 15:09:08 +0900

    fix : error during renaming name

--- ITS/src/main/java/com/example/its/swingUI/ProjectController.java
+++ ITS/src/main/java/com/example/its/swingUI/ProjectController.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import com.example.its.dataClassDB.ProjectDetailDB;
 
 //MainScene을 위한 기능이 담긴 interface입니다. 추후에 다른 이름으로 수정될 수 있습니다.
-public interface SwingController {
+public interface ProjectController {
     ArrayList<ProjectDetailDB> getProjectList();
     void makeNewProject(String title, String decs);
 }
