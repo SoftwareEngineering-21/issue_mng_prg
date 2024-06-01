6cd98b8b91012d08aea3c3cfb48a176497b88a97
CloudNru <grutugy1025@gmail.com>
2024-06-02 01:17:04 +0900

    create : graph.html

---
+++ ITS/src/main/java/com/example/its/webUI/Controller/User/GraphController.java
@@ -0,0 +1,35 @@
+package com.example.its.webUI.Controller.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GraphController {
+    @GetMapping("/graph")
+    public String graph(Model model) {
+        // 초기 데이터를 가져와 모델에 추가
+
+        String title = "A";
+        model.addAttribute("title", title);
+
+        double[] initialData = new double[] {70.0, 70.0, 70.0, 40.0, 50.0}; // 예시 함수 이름
+        model.addAttribute("initialData", initialData);
+        return "graph";
+    }
+
+    @PostMapping("/graph")
+    public String updateGraph(Model model) {
+        System.out.println("category");
+        String title = "My Chart Title";
+        model.addAttribute("title", title);
+
+        // 카테고리에 해당하는 데이터를 가져와 모델에 추가
+        double[] newData = new double[] {100.0, 100.0, 100.0, 100.0, 100.0}; // 예시 함수 이름
+        model.addAttribute("initialData", newData);
+
+        return "graph :: chart"; // Ajax로 업데이트하므로 일부분만 업데이트
+    }
+}
